68aefd17bcad6813ef3533e4a961f235da262191
JayaKeerthan01 <jjayakeerthan@gmail.com>
2023-10-29 01:45:13 +0530

    committed

---
+++ booksky.js
@@ -0,0 +1,52 @@
+var addpopupbutton=document.getElementById("add-popup-button")
+        var popupoverlay=document.querySelector(".popup-overlay")
+        var popupbox=document.querySelector(".popup-box")
+       
+//add button
+        addpopupbutton.addEventListener("click",function(){
+            popupoverlay.style.display="block"
+            popupbox.style.display="block"
+
+        })
+//cancel button
+
+        var cancelpopup=document.getElementById("cancel-popup")
+
+        cancelpopup.addEventListener("click",function(event){
+            event.preventDefault()
+            popupoverlay.style.display="none"
+            popupbox.style.display="none"
+        })
+//add book button
+       var container=document.querySelector(".container")
+       var addbook=document.getElementById("add-book")
+       var booktitleinput=document.getElementById("book-title-input")
+       var bookauthorinput=document.getElementById("book-author-input")
+       var bookdescriptioninput=document.getElementById("book-description-input")
+
+        addbook.addEventListener("click",function(event){
+                event.preventDefault()
+                var div=document.createElement("div")
+                div.setAttribute("class","book-container")
+                div.innerHTML=`<h2>${booktitleinput.value}</h2>
+                <h4>${ bookauthorinput.value}</h4>
+                <p>${bookdescriptioninput.value}</p>
+                <button onclick="deleteBook(event)">Delete</button>`
+                container.append(div)
+                popupoverlay.style.display="none"
+                popupbox.style.display="none"
+        })
+        
+
+        function deleteBook(event){
+                event.target.parentElement.remove()
+        }
+
+
+
+
+
+
+
+
+        
